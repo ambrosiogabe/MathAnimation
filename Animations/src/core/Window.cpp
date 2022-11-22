16dfb3806d64e6071cfdf79dd2e22d4915e35615
Gabe Ambrosio <ambrosiogabe@gmail.com>
2022-11-22 09:22:13 -0600

    Make my life better

--- Animations/src/core/Window.cpp
+++ Animations/src/core/Window.cpp
@@ -20,6 +20,7 @@ namespace MathAnim
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_SAMPLES, 4);
+		glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 
 		windowPtr = (void*)glfwCreateWindow(width, height, title, nullptr, nullptr);
 		if (windowPtr == nullptr)
